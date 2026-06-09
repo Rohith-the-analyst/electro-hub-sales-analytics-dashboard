025467e96ba2f385037f219c3c76d3d1a3f1912d
G ROHITH <rohithg0605@gmail.com>
2026-06-09 10:09:57 +0530

    Remove duplicate re-export

--- src/emailjsConfig.js
+++ src/emailjsConfig.js
@@ -8,6 +8,3 @@
 export const EMAILJS_SERVICE_ID  = 'service_amcbgsu'
 export const EMAILJS_TEMPLATE_ID = 'template_nhrfsli'
 export const EMAILJS_PUBLIC_KEY  = 'd8KAnRt0do8cPQDgF'
-
-
-export { EMAILJS_SERVICE_ID, EMAILJS_TEMPLATE_ID, EMAILJS_PUBLIC_KEY }
